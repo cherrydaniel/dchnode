368c0684371796dd6baa3c9daab242d17497029b
DanielCherry <archnovaelite@gmail.com>
2024-08-10 10:37:19 +0300

    rm files + add s3

---
+++ s3.js
@@ -0,0 +1,36 @@
+const {env} = require('process');
+const AWS = require('aws-sdk');
+const {wait} = require('./dchcore/util/concurrent.js');
+
+const E = module.exports;
+
+E.createInstance = ({region, accessKeyId, secretAccessKey})=>new AWS.S3({
+    region: region||env.BUCKETEER_AWS_REGION,
+    credentials: {
+        accessKeyId: accessKeyId||env.BUCKETEER_AWS_ACCESS_KEY_ID,
+        secretAccessKey: secretAccessKey||env.BUCKETEER_AWS_SECRET_ACCESS_KEY,
+    },
+});
+
+E.upload = (filepath, dataStream)=>{
+    const w = wait();
+    E.createInstance().upload({
+        Bucket: env.BUCKETEER_BUCKET_NAME,
+        Key: filepath,
+        Body: dataStream,
+    }, (err, data)=>{
+        if (err)
+            return void w.reject(err);
+        w.resolve(data);
+    });
+    return w.promise;
+};
+
+E.download = filepath=>{
+    return E.createInstance().getObject({
+        Bucket: env.BUCKETEER_BUCKET_NAME,
+        Key: filepath,
+    }).createReadStream();
+};
+
+// TODO: https://docs.aws.amazon.com/AWSJavaScriptSDK/v3/latest/Package/-aws-sdk-client-s3/Class/S3Client/
