6fe46a67817bf7719f992063a18d31e40dbc96c7
DanielCherry <archnovaelite@gmail.com>
2024-08-13 12:19:21 +0300

    add stream utils

--- stream.js
+++ stream.js
@@ -1,4 +1,5 @@
-const {Readable} = require('stream');
+const {Readable, Writable, Transform} = require('stream');
+const _ = require('lodash');
 const {wait} = require('./dchcore/concurrent.js');
 
 const E = module.exports;
@@ -20,3 +21,72 @@ E.readableStreamToString = async stream=>{
 };
 
 E.stringToStream = str=>Readable.from([str]);
+
+E.createReadable = (opt, handler)=>{
+    if (_.isFunction(opt)) {
+        handler = opt;
+        opt = {};
+    }
+    const rs = Readable(opt);
+    rs._read = async ()=>{
+        let pushed = false;
+        let result = await handler(v=>{
+            rs.push(v);
+            pushed = true;
+        });
+        if (!pushed)
+            rs.push(result);
+    };
+    return rs;
+};
+
+E.createTransform = (opt, handler)=>{
+    if (_.isFunction(opt)) {
+        handler = opt;
+        opt = {};
+    }
+    return Transform({
+        ...opt,
+        transform: async (chunk, enc, cb)=>{
+            let pushed = false;
+            try {
+                let result = await handler(chunk, enc, (e, v)=>{
+                    cb(e, v);
+                    pushed = true;
+                });
+                if (!pushed)
+                    cb(null, result);
+            } catch (e) {
+                cb(e);
+            }
+        },
+    })
+};
+
+E.createWritable = (opt, handler)=>{
+    if (_.isFunction(opt)) {
+        handler = opt;
+        opt = {};
+    }
+    const ws = Writable(opt);
+    ws._write = async (chunk, enc, next)=>{
+        let calledNext = false;
+        await handler(chunk, enc, ()=>{
+            next();
+            calledNext = true;
+        });
+        if (!calledNext)
+            next();
+    };
+    return ws;
+};
+
+E.createObjectReadable = handler=>E.createReadable({objectMode: true}, handler);
+
+E.createObjectTransform = handler=>E.createTransform({objectMode: true}, handler);
+
+E.createObjectWritable = handler=>E.createWritable({objectMode: true}, handler);
+
+E.createStringifyTransform = ()=>E.createObjectTransform(obj=>JSON.stringify(obj));
+
+E.createResWriter = res=>E.createWritable(chunk=>res.write(chunk)); // PROBABLY UNNEEDED!!!
