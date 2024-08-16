b7cc82afa4667a9dea0fbce89b9bd375458ab66c
DanielCherry <archnovaelite@gmail.com>
2024-08-16 14:37:08 +0300

    fix

--- api.js
+++ api.js
@@ -69,7 +69,7 @@ E.mwHandle = fn=>(req, res, next)=>{ (async ()=>{
 })(); };
 
 E.mwValidate = schema=>E.mwHandle(req=>{
-    const {error} = joi.object.keys(schema).validate(req.allParams)||{};
+    const {error} = joi.object().keys(schema).validate(req.allParams)||{};
     if (error)
         throw E.err(`Validation error: ${error.details.map(e=>e.message).join(', ')}.`,
             'validation_error', 400);
